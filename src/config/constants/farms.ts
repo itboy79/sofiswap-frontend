facd6ad4138f36dbd0792902385af9cd9f4c24e4
itboy79 <62352832+itboy79@users.noreply.github.com>
2021-07-06 12:15:53 +0200

    a

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -7,7 +7,7 @@ const farms: FarmConfig[] = [
    */
   {
     pid: 0,
-    lpSymbol: 'CAKE',
+    lpSymbol: 'cazzo',
     lpAddresses: {
       97: '0x9C21123D94b93361a29B2C2EFB3d5CD8B17e0A9e',
       56: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
